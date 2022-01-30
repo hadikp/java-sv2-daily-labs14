141f77a6bca40b3c2dae88847e29c4cce6f7f9be
hadikp <hp20001025@gmail.com>
2022-01-30 14:45:36 +0100

    day03 BankAccount

---
+++ week14-lab-tasks/src/main/java/day03/BankAccounts.java
@@ -0,0 +1,34 @@
+package day03;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class BankAccounts {
+
+    private List<BankAccount> bankAccounts;
+
+    public BankAccounts(List<BankAccount> bankAccounts) {
+        this.bankAccounts = bankAccounts;
+    }
+
+    public List<BankAccount> orderingByBankAccount() {
+        List<BankAccount> result = new ArrayList<>(bankAccounts);
+        Collections.sort(result, Comparator.comparing(BankAccount::getAccountNumber));
+        return result;
+    }
+
+    public List<BankAccount> orderingByBalance() {
+        List<BankAccount> result = new ArrayList<>(bankAccounts);
+        Comparator<BankAccount> comparator = Comparator.comparing(BankAccount::getBalance);
+        result.sort(comparator.reversed());
+        return result;
+    }
+
+    public List<BankAccount> orderingByNameAndBankAccount() {
+        List<BankAccount> result = new ArrayList<>(bankAccounts);
+        result.sort((Comparator.comparing(BankAccount::getNameOfOwner, Comparator.nullsFirst(Comparator.naturalOrder())).thenComparing(BankAccount::getAccountNumber)));
+        return result;
+    }
+}
